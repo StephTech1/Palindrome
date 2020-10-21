34608704dc953ebdc147c8286c37bf3183afff64
Steph St <steph.streete@outlook.com>
2020-10-21 20:04:23 +0000

    started code

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #Write a program that takes in a word and says whether or not it is a palindrome
+print ("Is your word a Palindrome?")
+word = input("What is your word?")
+
+if word = 
+print ("Yes!")
+elif word = 
+print("No!")
