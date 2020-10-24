67ea3173e95de37d3e5847b766f651b3c6221a12
Steph St <steph.streete@outlook.com>
2020-10-24 17:02:55 +0000

    Added more to code if and elif statements

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #Write a program that takes in a word and says whether or not it is a palindrome
 print ("Is your word a Palindrome?")
-word = input("What is your word?")
+word = str(input("What is your word?"))
 
-if word = 
-print ("Yes!")
-elif word = 
-print("No!")
+
+if (word == "input reversed"):
+  print("Yes!")
+
+elif (word != input):
+  print("No!")
+
+else (word )
+  print("Thanks for playing"):
