c3503fb3197c0cc4c2a03a3ed3210f2afa5665fa
Steph St <steph.streete@outlook.com>
2020-10-24 19:28:27 +0000

    added to code - need to work out reverse function

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #Write a program that takes in a word and says whether or not it is a palindrome
+#Add title to code
 print ("Is your word a Palindrome?")
-word = str(input("What is your word?"))
+#Ask user for word
+input = str(input("What is your word?"))
+#create a function to check if a string is reversed
+def reverse_word (palin):
+  return palin == palin  
 
+palin = input 
 
-if (word == "input reversed"):
+if (reverse_word) :
   print("Yes!")
 
-elif (word != input):
+else:
   print("No!")
 
-else (word )
-  print("Thanks for playing"):
+#else:
+  #print("Thanks for playing!")
