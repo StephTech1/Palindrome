ea3d9f6ce4d1889d17bff7c6cd988dca88c09efa
Steph St <steph.streete@outlook.com>
2020-10-25 18:14:28 +0000

    tested code

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
-#Write a program that takes in a word and says whether or not it is a palindrome
 #Add title to code
 print ("Is your word a Palindrome?")
 #Ask user for word
 input = str(input("What is your word?"))
+palin = input
 #create a function to check if a string is reversed
 #end to beginning counting down by 1
-
-
-palin = input 
-
 if palin == palin [::-1] :
+#print answers based on input
   print("Yes!")
-
 else:
   print("No!")
-
-#else:
-  #print("Thanks for playing!")
+print("Thanks for playing!")
